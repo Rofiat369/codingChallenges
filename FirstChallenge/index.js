11744ca16271e11c360d50568e0b636a143e327d
Rofiat <rofiatolusanya12@gmail.com>
2022-04-11 13:36:21 -0400

    Created a variables to store data

--- FirstChallenge/index.js
+++ FirstChallenge/index.js
@@ -7,3 +7,9 @@ let admin;
 let name = "John";
 admin = name;
 alert(admin);
+
+// Create a variable with the name of our planet. How would you name such a variable?
+// Create a variable to store the name of a current visitor to a website. How would you name that variable?
+
+const planetName = "earth";
+let currentVisitorName = "name";
