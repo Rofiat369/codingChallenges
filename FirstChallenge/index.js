4353fac8b131be89750157a946de285371a25f39
Rofiat <rofiatolusanya12@gmail.com>
2022-04-15 11:40:34 -0400

    added a loop that ask user for number input then displays the multiplication table for that number

--- FirstChallenge/index.js
+++ FirstChallenge/index.js
@@ -1,42 +1,42 @@
-// Declare two variables: admin and name.
-// Assign the value "John" to name.
-// Copy the value from name to admin.
-// Show the value of admin using alert (must output “John”).
+// // Declare two variables: admin and name.
+// // Assign the value "John" to name.
+// // Copy the value from name to admin.
+// // Show the value of admin using alert (must output “John”).
 
-let admin;
-let name = "John";
-admin = name;
-alert(admin);
+// let admin;
+// let name = "John";
+// admin = name;
+// alert(admin);
 
-// Create a variable with the name of our planet. How would you name such a variable?
-// Create a variable to store the name of a current visitor to a website. How would you name that variable?
+// // Create a variable with the name of our planet. How would you name such a variable?
+// // Create a variable to store the name of a current visitor to a website. How would you name that variable?
 
-const planetName = "earth";
-let currentVisitorName = "name";
+// const planetName = "earth";
+// let currentVisitorName = "name";
 
-// Write a function that returns true if the parameter age is greater than 18. Otherwise it asks for a confirmation and returns its result.
+// // Write a function that returns true if the parameter age is greater than 18. Otherwise it asks for a confirmation and returns its result.
 
-function checkAge(age){
- return (age > 18) ? true : confirm('Did parents allow you?')
-}
+// function checkAge(age){
+//  return (age > 18) ? true : confirm('Did parents allow you?')
+// }
 
-function checkAge(age){
-    return (age > 18) || confirm("Did parents allow you?")
-}
+// function checkAge(age){
+//     return (age > 18) || confirm("Did parents allow you?")
+// }
 
-// Write a function min(a,b) which returns the least of two numbers a and b.
+// // Write a function min(a,b) which returns the least of two numbers a and b.
 
-function returnMin(a, b){
-return (a < b) ? a : b
-}
+// function returnMin(a, b){
+// return (a < b) ? a : b
+// }
 
-console.log(returnMin (4, 9))
+// console.log(returnMin (4, 9))
 
-// function that returns a the function yes or no based on user agreement to question
-let ask = (question, yes, no) => confirm(question)? yes() : no();
-  ask(
-    "Do you agree?",
-    () => alert("You agreed."),
-    () => alert("You canceled the execution.")
-  );
+// // function that returns a the function yes or no based on user agreement to question
+// let ask = (question, yes, no) => confirm(question)? yes() : no();
+//   ask(
+//     "Do you agree?",
+//     () => alert("You agreed."),
+//     () => alert("You canceled the execution.")
+//   );
 
--- Loops/index.js
+++ Loops/index.js
@@ -5,37 +5,49 @@
 // When it's done, improve it so that the program shows "FizzBuzz" instead for numbers divisible both by 3 and by 5.
 
 //numbers between 1-100
-for(i=1; i < 101; i++){
-    //check if the number is divisible by both 3 & 5
-    if(i % 3 === 0 && i % 5 === 0){
-        console.log("fizzbuzz")
-    } 
-    //check if number is divisible by 5
-    else if(i % 5 === 0){
-        console.log("buzz")
-    }
-    //check if number is divisible by 3
-    else if(i % 3 === 0){
-        console.log("fizz")
-    }
-    else{
-        console.log(i)
-    }
+for (i = 1; i < 101; i++) {
+  //check if the number is divisible by both 3 & 5
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("fizzbuzz");
+  }
+  //check if number is divisible by 5
+  else if (i % 5 === 0) {
+    console.log("buzz");
+  }
+  //check if number is divisible by 3
+  else if (i % 3 === 0) {
+    console.log("fizz");
+  } else {
+    console.log(i);
+  }
 }
 
 //Write a program that continues to ask the user for a number until the entered number is less than or equal to 100.
 //When you are done with the above, improve the program so that the terminating number is between 50 and 100.
 
-let num  = prompt("Enter a number greater than 100?");
+let num = prompt("Enter a number greater than 100?");
 
-  while (num <= 100 && num){
-    if (num < 50){
-      num =prompt("Enter a number greater than 100?");
-    }
-    else if(num === null || num === ""){
-      num =prompt("Enter a number greater than 100?");
-    } 
-    else if(num > 50 && num < 100){
+while (num <= 100 && num) {
+  if (num < 50) {
+    num = prompt("Enter a number greater than 100?");
+  } else if (num === null || num === "") {
+    num = prompt("Enter a number greater than 100?");
+  } else if (num > 50 && num < 100) {
     break;
-    }
   }
+}
+
+// Write a program that asks the user for a number, then shows the multiplication table for this number.
+// When you are done, improve the program so it only accepts numbers between 2 and 9 (use the previous exercise as a blueprint).
+
+let userInput = prompt("enter a num");
+for (let i = 1; i < 13; ++i) {
+    if(userInput > 1 && userInput < 10){
+        document.write(
+            userInput + " " + "*" + " " + i + " " + "=" + " " + userInput * i + "<br>"
+          );
+    } else {
+        document.write("<h4> please enter a number between 2 - 9 </h4>")
+    }
+ 
+}
