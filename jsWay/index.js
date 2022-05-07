46b893210eab9667a611dc9c55dba859d81dfc4f
Rofiat <rofiatolusanya12@gmail.com>
2022-05-06 22:17:37 -0400

    got inputs from user and turned them in lower case

---
+++ jsWay/index.js
@@ -0,0 +1,13 @@
+//Write a program that accepts an input of day from the user
+//then shows the user the name of the following day
+
+// global array variable with each day of the week and set it up to where
+// if the user input is included in the array exactly, print that next day in the array by the index
+// days [dayIndex + 1]
+
+document.querySelector(".entered").addEventListener("click", (e) => {
+  let inputs = document.querySelector(".word").value;
+  console.log(inputs);
+});
+
+//const dayOfWeek = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
