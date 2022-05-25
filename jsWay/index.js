2ec559ddf188cf060edf54aa1b1b1ef9bce37680
Rofiat <rofiatolusanya12@gmail.com>
2022-05-25 18:35:46 -0400

    created a function that takes an input from user and display an input to the user

--- jsWay/index.js
+++ jsWay/index.js
@@ -1,13 +1,30 @@
 //Write a program that accepts an input of day from the user
 //then shows the user the name of the following day
 
-// global array variable with each day of the week and set it up to where
-// if the user input is included in the array exactly, print that next day in the array by the index
-// days [dayIndex + 1]
-
 document.querySelector(".entered").addEventListener("click", (e) => {
   let inputs = document.querySelector(".word").value.toLowerCase();
-  console.log(inputs);
+  switch(true){
+    case inputs === "monday":
+     return document.getElementById('placeText').textContent = "Tuesday";
+
+    case inputs === "tuesday":
+      return document.getElementById('placeText').textContent = "Wednesday"
+
+    case inputs === "wednesday":
+      return document.getElementById('placeText').textContent = "Thursday"
+
+    case inputs === "thursday":
+      return document.getElementById('placeText').textContent = "Friday"
+
+    case inputs === "friday":
+      return document.getElementById('placeText').textContent ="Saturday"
+
+    case inputs === "saturday":
+      return document.getElementById('placeText').textContent = "Sunday"
+
+    case inputs === "sunday":
+      return document.getElementById('placeText').textContent = "Monday"
+  }
 });
 
 //const dayOfWeek = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
